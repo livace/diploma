079a780b4920e15f22045a6065306f90437eb109
Alexey Ilyukhov <l1vac3@gmail.com>
2022-04-21 19:26:52 +0300

    Cmake

---
+++ cmake/conan.cmake
@@ -0,0 +1,35 @@
+include_guard(GLOBAL)
+
+message("running conan install")
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+  message("compiler: clang")
+  set(CONAN_PROFILE "${CMAKE_CURRENT_LIST_DIR}/clang.profile")
+else()
+  message(FATAL_ERROR "unknown compiler: ${CMAKE_CXX_COMPILER_ID}")
+endif()
+
+find_program(CONAN conan)
+if(NOT CONAN)
+  message(FATAL_ERROR "conan not found")
+endif()
+
+set(ENV{CONAN_REVISIONS_ENABLED} 1)
+
+set(CONANFILE "${ROOT}/cmake/conanfile.txt")
+execute_process(
+  RESULT_VARIABLE CONAN_RETURN_CODE
+  OUTPUT_FILE "${CMAKE_BINARY_DIR}/conan.log"
+  ERROR_FILE "${CMAKE_BINARY_DIR}/conan.log"
+  COMMAND "${CONAN}" "install"
+      "${CONANFILE}"
+      "--install-folder" "${CMAKE_BINARY_DIR}"
+      "--profile" "${CONAN_PROFILE}"
+      "--build=missing"
+)
+
+if(NOT CONAN_RETURN_CODE EQUAL 0)
+  message(FATAL_ERROR "conan install failed")
+endif()
+
+message("conan installed successfully")
---
+++ generators/generator_cli.cpp
@@ -0,0 +1,1 @@
+#include "radnom_generator.cpp"
